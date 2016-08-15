fa665eff7272e062bab33629c4a5285169f62f3e
batsauto <cfbarrow@gmail.com>
2016-08-14 18:36:59 -0700

    finished some of the functions assignment... i am moving on and will come back later to finish it

---
+++ jQuery/functions/function.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    //your jQuery code goes here
+    $('#click > div > button').click(function() {
+        alert("handler for .click() called");
+    });
+
+    $('#focus > div > button').focus(function() {
+        $(this).next('p').css('display', 'inline').fadeOut( 2000 );
+    });
+    $('#addclass > div > button').click(function() {
+        $(this).next('p').addClass('redbackground');
+    });
+
+    function displayVals() {
+        var singleValues = $( "#single" ).val();
+
+        $( "#val > div >p" ).html( "Select to see the .val of<b> Single:</b> " + singleValues );
+    }
+
+    $( "select" ).change( displayVals );
+    displayVals();
+
+    $('#text > div > button').click(function() {
+        var str = $( "#text > div > p:first" ).text();
+        $( "#text > div > p:last" ).html( str ).css('display', 'block');
+    });
+
+    $('#attr > div > button').click(function() {
+        var title = $( "#attr > div > iframe" ).attr( "src" );
+        $( "#attr > div > p" ).text( title );
+    });
+
+});
