0c6d805b94356d7caa29c9b020f40c05dede4f3a
batsauto <cfbarrow@gmail.com>
2016-08-20 11:40:11 -0700

    finished the contactcard assignment without the show hide functionality working

---
+++ jQuery/contactCard/README.txt
@@ -0,0 +1,6 @@
+I really took a while to get this assignment working.
+
+I am having a hard time getting the show and hide function to
+work independently of the .on(submit) function. I also didn't
+do any styling again because of the amount of time that i have
+already spent on it.
--- jQuery/contactCard/contactCard.js
+++ jQuery/contactCard/contactCard.js
@@ -1,28 +1,20 @@
 $(document).ready(function() {
-    $('form').submit(function() {
+
+    var idnum = 0;
+    $('form').on('submit',(function() {
+
         $('#contactContainer').append("<div>" + "</div>");
         $('#contactContainer > div').addClass("contactCard");
+        $('.contactCard').last().attr('id', idnum++ );
+        $('.contactCard').last().append( "<h3>" + $("#first_name").val()+ " " + $("#last_name").val() + "</h3>" + "<p>" + $("#description").val() + "</p>" );
+
+        // I am having a hard time getting the show and hide functinalities to work indepdently of the .on(submit) function.
+
+        //$('.contactCard > p').hide();
+        // $('.contactCard').click(function() {
+        //     $('.contactCard > p').show();
+        // });
         return false;
-    });
-    $('form').on('submit',(function() {
-    $(".contactCard").append("<h3>"
-				+ $("#first_name").val()
-				+ $("#last_name").val()
-        + "</h3>"
-        + "<p>"
-        +  $("#description").val()
-        + "</p>"
-
-
-
-    );
     }));
-
-
-
-
-
-
-
 
 });
