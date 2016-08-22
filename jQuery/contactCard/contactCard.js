77c4dcce22c86d9b8480dcf3c48f36f37640c208
batsauto <cfbarrow@gmail.com>
2016-08-22 11:04:41 -0700

    finished a few assignments

--- jQuery/contactCard/contactCard.js
+++ jQuery/contactCard/contactCard.js
@@ -1,20 +1,13 @@
 $(document).ready(function() {
 
-    var idnum = 0;
+    // var idnum = 0;
     $('form').on('submit',(function() {
+        $('#contactContainer').append( "<div class='contactCard'><h3>" + $("#first_name").val()+ " " + $("#last_name").val() + "</h3>" + "<p style='display:none'>" + $("#description").val() + "</p></div>" );
 
-        $('#contactContainer').append("<div>" + "</div>");
-        $('#contactContainer > div').addClass("contactCard");
-        $('.contactCard').last().attr('id', idnum++ );
-        $('.contactCard').last().append( "<h3>" + $("#first_name").val()+ " " + $("#last_name").val() + "</h3>" + "<p>" + $("#description").val() + "</p>" );
-
-        // I am having a hard time getting the show and hide functinalities to work indepdently of the .on(submit) function.
-
-        //$('.contactCard > p').hide();
-        // $('.contactCard').click(function() {
-        //     $('.contactCard > p').show();
-        // });
         return false;
     }));
+    $(document).on("click", ".contactCard", function(){
+        $(this).children().toggle();
+    });
 
 });
