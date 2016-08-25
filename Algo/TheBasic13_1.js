a1df3498a0e2efaa99e558fec1943e5762593577
batsauto <cfbarrow@gmail.com>
2016-08-24 21:34:00 -0700

    renamed some files and added the pacman project

--- Algo/TheBasic13_1.js
+++ Algo/TheBasic13_1.js
@@ -59,7 +59,7 @@ for (var i = 1; i < arr.length; i++ ){
 console.log(arr);
 
 //Greater than Y
-var Y = 6
+var Y = 6;
 var arr = [0,1,2,3,4,5,6,7,8,9];
 for ( var i =0; i < arr.length; i++)
     if (i > 6){
