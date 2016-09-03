adde847cef245940ddeb25796cf5f0565dc668b4
batsauto <cfbarrow@gmail.com>
2016-09-03 09:12:36 -0700

    added python folder with the first two assginments

---
+++ Python/multiples/multiples.py
@@ -0,0 +1,12 @@
+# part I
+for countOdd in range (0, 1000):
+    if countOdd % 2 == 0:
+        continue
+    print countOdd
+
+
+# part II
+multi = 5
+while multi < 1000001:
+    print multi
+    multi += 5
