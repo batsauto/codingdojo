a105ca91eb27c7e5c7c89db56e7a7776b0d35ad7
batsauto <cfbarrow@gmail.com>
2016-09-06 15:58:21 -0700

    finished the mulitply and started the scores assginment.

---
+++ Python/basics/multiply.py
@@ -0,0 +1,6 @@
+a = [2,4,10,16]
+def multiply(a):
+    a = [5 * x for x in a]
+    return a
+result = multiply(a)
+print result
---
+++ Python/basics/scores.py
@@ -0,0 +1,38 @@
+def grade(g):
+    if raw_input < 59:
+        g = "Bad Bongos!"
+    elif raw_input >= 60 and raw_input < 70:
+        g = "D"
+    elif raw_input >= 70 and raw_input < 80:
+        g = "C"
+    elif raw_input >= 80 and raw_input < 90:
+        g = "B"
+    elif raw_input >= 90 and raw_input <= 100:
+        g = "A"
+    print g
+
+
+print "Please enter the scores:"
+
+print "What is your 1st score?",
+score1 = raw_input()
+print "Your grade is:" grade(score1)
+
+print "What is your 2nd score?",
+score2 = raw_input()
+print "What is your 3rd score?",
+score3 = raw_input()
+print "What is your 4th score?",
+score4 = raw_input()
+print "What is your 5th score?",
+score5 = raw_input()
+print "What is your 6th score?",
+score6 = raw_input()
+print "What is your 7th score?",
+score7 = raw_input()
+print "What is your 8th score?",
+score8 = raw_input()
+print "What is your 9th score?",
+score9 = raw_input()
+print "What is your 10th score?",
+score10 = raw_input()
