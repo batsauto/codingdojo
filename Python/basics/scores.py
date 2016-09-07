3db2ab6a154ae1b16f695283db669c2cf919fb78
batsauto <cfbarrow@gmail.com>
2016-09-06 22:31:35 -0700

    finished the scores assignment

--- Python/basics/scores.py
+++ Python/basics/scores.py
@@ -1,58 +1,52 @@
-def grade(g):
-    if raw_input < 59:
-        g = "Bad Bongos!"
-    elif raw_input >= 60 and raw_input < 70:
-        g = "D"
-    elif raw_input >= 70 and raw_input < 80:
-        g = "C"
-    elif raw_input >= 80 and raw_input < 90:
-        g = "B"
-    elif raw_input >= 90 and raw_input <= 100:
-        g = "A"
-    print g
+def grader(score):
+    if (score < 59):
+        letter_grade = "Bad Bongos!"
+    elif (score >= 60 and score < 70):
+        letter_grade = "D"
+    elif (score >= 70 and score < 80):
+        letter_grade = "C"
+    elif (score >= 80 and score < 90):
+        letter_grade = "B"
+    elif (score >= 90 and score <= 100):
+        letter_grade = "A"
+    print "Your Score is {}. Your grade is: {}".format(score, letter_grade)
 
 
 print "Please enter the scores:"
 
 print "What is your 1st score?",
-score1 = raw_input()
-grade1 = grade(score1)
-print "Your grade is:" grade1
+score = input()
+# score = int(raw_input())
+# either one above will work... i just chose this way.
+grader(score)
 
 print "What is your 2nd score?",
-score2 = raw_input()
+score = input()
+grader(score)
 print "What is your 3rd score?",
-score3 = raw_input()
+score = input()
+grader(score)
 print "What is your 4th score?",
-score4 = raw_input()
+score = input()
+grader(score)
 print "What is your 5th score?",
-score5 = raw_input()
+score = input()
+grader(score)
 print "What is your 6th score?",
-score6 = raw_input()
+score = input()
+grader(score)
 print "What is your 7th score?",
-score7 = raw_input()
+score = input()
+grader(score)
 print "What is your 8th score?",
-score8 = raw_input()
+score = input()
+grader(score)
 print "What is your 9th score?",
-score9 = raw_input()
+score = input()
+grader(score)
 print "What is your 10th score?",
-score10 = raw_input()
+score = input()
+grader(score)
 
-from random import randint
 
-print "Scores and Grades"
-for count in range(0, 10):
-	score = randint(70, 100)
-
-	if(score <= 70):
-		grade = "D"
-	elif(score <= 80):
-		grade = "C"
-	elif(score <= 90):
-		grade = "B"
-	else:
-		grade = "A"
-
-	print "Score: %d; Your grade is %s" %(score, grade)
-
-print "End of program. Bye!"
+print "End of Program. Bye!"
