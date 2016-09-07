fd95b1a11f3ae2cdf4ca92260a176038bcf9381b
batsauto <cfbarrow@gmail.com>
2016-09-06 17:36:59 -0700

    added some info to the git ignore and the scores assginmnet.

--- Python/basics/scores.py
+++ Python/basics/scores.py
@@ -16,7 +16,8 @@ print "Please enter the scores:"
 
 print "What is your 1st score?",
 score1 = raw_input()
-print "Your grade is:" grade(score1)
+grade1 = grade(score1)
+print "Your grade is:" grade1
 
 print "What is your 2nd score?",
 score2 = raw_input()
@@ -36,3 +37,22 @@ print "What is your 9th score?",
 score9 = raw_input()
 print "What is your 10th score?",
 score10 = raw_input()
+
+from random import randint
+
+print "Scores and Grades"
+for count in range(0, 10):
+	score = randint(70, 100)
+
+	if(score <= 70):
+		grade = "D"
+	elif(score <= 80):
+		grade = "C"
+	elif(score <= 90):
+		grade = "B"
+	else:
+		grade = "A"
+
+	print "Score: %d; Your grade is %s" %(score, grade)
+
+print "End of program. Bye!"
