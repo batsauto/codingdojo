21c9d55055068f1318230489c70460e7c6e84a85
batsauto <cfbarrow@gmail.com>
2016-09-17 11:38:39 -0700

    added some form validation the dojoSurvey

--- flaskFundamentals/dojoSurvey/server.py
+++ flaskFundamentals/dojoSurvey/server.py
@@ -1,19 +1,25 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, flash, session, render_template, request, redirect, url_for
 app = Flask(__name__)
+app.secret_key = 'very secret'
 
 
 @app.route('/')
-def homeView():
+def index():
 	return render_template('index.html')
 
 
-@app.route('/results', methods=['POST'])
-def create_user():
-    return render_template('results.html',
-        name=request.form['name'],
-        location=request.form['location'],
-        favLang=request.form['favLang'],
-        comments=request.form['comments'])
-    return redirect('/')
+@app.route('/process', methods=['POST'])
+def process():
+	if len(request.form['name']) < 1 or len(request.form['comments']) < 1:
+		flash("you need to fill out the name and comments")
+	else:
+		return render_template('results.html', name=request.form['name'],
+			location=request.form['location'], favLang=request.form['favLang'],
+		    comments=request.form['comments'])
+	return redirect('/')
+
+
+# @app.route('/results', methods=['POST'])
+
 
 app.run(debug=True)
