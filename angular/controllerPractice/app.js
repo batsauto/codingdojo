261a01d1251aa4d75608b0a1b2f411928e7fd579
Christian Barrow <cfbarrow@gmail.com>
2016-12-06 20:33:18 -0800

    started the controller practice

---
+++ angular/controllerPractice/app.js
@@ -0,0 +1,14 @@
+var myAppModule = angular.module('myApp', []);
+
+myAppModule.controller('productsController', function($scope){
+  $scope.foods = [
+              // {name: 'Pizza'},
+              // {name: 'Salad'},
+              // {name: 'Chicken'},
+              // {name: 'Sandwhich'}
+        ];
+  $scope.addFood = function(){
+    $scope.foods.push($scope.newFood);
+    $scope.newFood = {};
+  }
+});
